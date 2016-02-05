15c345e4ef26c0704726e095f0daa8cf977f7cd4
CorvusCoraks <gremal@mail.ru>
2016-02-05 21:53:27 +0300

    Полностью функционирует. Зафиксировать версию. 2

--- src/start.java
+++ src/start.java
@@ -1,24 +1,28 @@
 import java.io.File;
 import java.io.IOException;
+// import java.util.Arrays;
 
 /**
  * Created by GreMal on 30.05.2015.
  */
 /*
-* Программа запуска CoudNotes. Если обнаруживает в директории программы новую версию, то удаляет старую
-* и переименовывает обновлённый файл в CloudNotes.jar
+* РџСЂРѕРіСЂР°РјРјР° Р·Р°РїСѓСЃРєР° CoudNotes. Р•СЃР»Рё РѕР±РЅР°СЂСѓР¶РёРІР°РµС‚ РІ РґРёСЂРµРєС‚РѕСЂРёРё РїСЂРѕРіСЂР°РјРјС‹ РЅРѕРІСѓСЋ РІРµСЂСЃРёСЋ, С‚Рѕ СѓРґР°Р»СЏРµС‚ СЃС‚Р°СЂСѓСЋ
+* Рё РїРµСЂРµРёРјРµРЅРѕРІС‹РІР°РµС‚ РѕР±РЅРѕРІР»С‘РЅРЅС‹Р№ С„Р°Р№Р» РІ CloudSticker.jar
 *
 * */
 public class start {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         File newFile = new File("./lastversion.jar");
-        File oldFile = new File("./CloudNotes.jar");
-        if(newFile.exists()){
-            if(oldFile.exists()){
-                if(oldFile.delete()){ newFile.renameTo(oldFile); }
+        File oldFile = new File("./CloudSticker.jar");
+
+        if(newFile.exists()) {
+            if (oldFile.exists()) {
+                if (oldFile.delete()) {
+                    newFile.renameTo(oldFile);
+                }
             }
         }
-        try{ Process proc = Runtime.getRuntime().exec("java -jar CloudNotes.jar start"); }
-        catch(IOException ignore){/*NOP*/}
+
+        Process proc = Runtime.getRuntime().exec("java -jar CloudSticker.jar start");
     }
 }
